52079db2287e4ec3452e7f38bbcef1557c6f23db
Ornzas <119805917+Ornzas@users.noreply.github.com>
2026-05-07 13:24:21 +0300

    Clean up Tp-linkConfig: remove hardcoded paths and debug cruft

--- Tp-linkConfig/tplinkConfig.py
+++ Tp-linkConfig/tplinkConfig.py
@@ -1,54 +1,19 @@
-import time
-#time.sleep(2)
-with open ('C:\\Users\\kasatkindi\\Desktop\\Bascket\\pyAuto\\Config.txt', 'r') as f:
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(script_dir, 'Config.txt'), 'r') as f:
     config = f.read()
-#f = open('C:\\Users\\kasatkindi\\Desktop\\Bascket\\pyAuto\\Config.txt', 'r')
-#config = f.read()
-f.close()
-#time.sleep(2)
-#print(config)
-#{ipGateway}
-#{ipAddress} {netMask}
-#time.sleep(1)
-with open("C:\\Users\\kasatkindi\\Desktop\\Bascket\\pyAuto\\ipAddresses.txt", "r") as f:
-    for ip in f:
-        #print("dghdh")
-        #input()
-        #ipAddress = ip.replace("\t","")
-        ipAddressArray = ip.split("\t")
-        #input()
-        #print(ipAddressArray)
-        #input()
-        #time.sleep(4)
-        ipAddress = ipAddressArray[0] + ipAddressArray[1]
-        ipAddress = ipAddress.replace("\n","")
-        ipAddress = ipAddress.replace("\r","")
-        #print("ip " + ipAddress)
-        #input("dfhdhd___")
-        lastOctet = int(ipAddressArray[1])
-        lastOctet -= 1
-        #input("dfhdhd_+_")
-        gateway = ipAddressArray[0] + str(lastOctet)
-        #print("gw " + gateway)
-        newConfig = config.replace('{ipAddress}', ipAddress)
-        newConfig = newConfig.replace('{ipGateway}', gateway)
-        f = open(ipAddress + '.txt', 'w')
-        for line in newConfig:
-            f.write(line)
-        f.close()
-        #print(newConfig)
 
-        print("new_config for ip: " + ipAddress)
-
-
-        
-##with open("C:\\Users\\kasatkindi\\Desktop\\Bascket\\pyAuto\\Config.txt", "r") as f:
-##    for line in f:
-##        #print(config + line)
-##        print(line.replace('{ipGateway}', '{ipGateway__}'))
-
-#print ("+++++++++++++++++++++++++++++++++++++")
-#print (config)
-#f.close()
-#time.sleep(1)
-input()
+with open(os.path.join(script_dir, 'ipAddresses.txt'), 'r') as f:
+    for line in f:
+        parts = line.strip().split('\t')
+        if len(parts) < 2:
+            continue
+        ip_address = parts[0] + parts[1]
+        gateway = parts[0] + str(int(parts[1]) - 1)
+        new_config = config.replace('{ipAddress}', ip_address).replace('{ipGateway}', gateway)
+        out_path = os.path.join(script_dir, ip_address + '.txt')
+        with open(out_path, 'w') as out:
+            out.write(new_config)
+        print(f"Config generated for: {ip_address}")
